e6c6f54efb98b72ad332de0dafc280e1d28425f6
Jesus Perez <jesuspereztao@gmail.com>
2025-06-29 22:48:25 -0400

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("formPago");
+    if (form) {
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        form.style.display = "none";
+        document.getElementById("mensaje").style.display = "block";
+    });
+    }
+});
